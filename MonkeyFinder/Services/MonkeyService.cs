4fa40d42a98b1c412ee383b0f5ad1782624389f9
obixander <noahhauge@gmail.com>
2024-05-27 14:58:03 +0200

    I did stuff i dont know if it works

--- MonkeyFinder/Services/MonkeyService.cs
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,28 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    HttpClient httpClient;
+    public MonkeyService()
+    {
+        httpClient = new HttpClient();
+    }
+
+    List<Monkey> monkeyList = new ();
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 1)
+            return monkeyList;
+
+        var json = "https://montemagno.com/monkeys.json";
+        var response = await httpClient.GetAsync(json);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+        return monkeyList;
+    }
 }
